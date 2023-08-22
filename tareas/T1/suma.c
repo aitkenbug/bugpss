4f3a9d01cfaea70014173a3de345f13e097b5fce
aitkenbug <vicente.aitken@ug.uchile.cl>
2023-08-22 17:34:32 -0400

    first working attempt

--- tareas/T1/suma.c
+++ tareas/T1/suma.c
@@ -6,10 +6,29 @@ Bcd mask = 0xf;
 Bcd sumaBcd(Bcd x, Bcd y) {
     Bcd x_mask;
     Bcd y_mask;
-    Bcd sum;
-    for (int i = 0, i<sizeof(Bcd), i++) {
+    Bcd sum = 0;
+    Bcd tmp;
+    Bcd exs;
+    int shift = 0;
+    for (int i = 0; i<sizeof(Bcd); i++) {
+        x = x >> shift;
+        y = y >> shift;
+        sum = sum << shift;
         x_mask = x & mask;
         y_mask = y & mask;
-        x_mask + y_mask 
+        tmp = x_mask + y_mask;
+        exs = tmp - 0xa;
+        if (tmp > 0x9) {
+            sum += exs;
+            sum += 0x10;
+        }
+        else {
+            sum += tmp;
+        }
+        
+        shift += 4;
+        i++;
     }
+    return sum;
 }
+
