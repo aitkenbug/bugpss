db4a9d2ff2f40121195ee21b381eae8782c8efa3
aitkenbug <vicente.aitken@ug.uchile.cl>
2023-08-24 10:01:51 -0400

    suma with while

--- tareas/T1/suma.c
+++ tareas/T1/suma.c
@@ -4,12 +4,13 @@
 Bcd mask = 0xf;
 
 Bcd sumaBcd(Bcd x, Bcd y) {
+    Bcd sum = 0;
     Bcd x_mask;
     Bcd y_mask;
-    Bcd sum = 0;
     Bcd tmp;
     Bcd exs = 0;
-    for (int shift = 0; shift<64; shift+=4) {
+    int shift = 0;
+    while (shift < 64) {
         x_mask = (x >> shift) & mask;
         y_mask = (y >> shift) & mask;
         tmp = x_mask + y_mask + exs;
@@ -21,8 +22,8 @@ Bcd sumaBcd(Bcd x, Bcd y) {
             sum = sum + (tmp << shift);
             exs = 0x0;
         }
+        shift += 4;
     }
-
     if (exs > 0) {
         return 0xffffffffffffffff;
     }
