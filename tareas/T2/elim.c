5332fa30fb7d6d0444f48a6b89bbb4464fcdcde9
aitkenbug <vicente.aitken@ug.uchile.cl>
2023-09-04 14:03:57 -0300

    no segfault

--- tareas/T2/elim.c
+++ tareas/T2/elim.c
@@ -5,16 +5,24 @@
 #include "elim.h"
 
 void eliminar(char *str, char *pat) {
-    char *s = *str;
-    char *p = *pat;
-    char *old = *str;
-    while (*p != 0) {
-        if (*s == *p) {
+    char *s = str;
+    char *p = pat;
+    char *old = str;
+    while (*s != 0) {
+        if (*s != *p) {
+            s++;
+            old++;
+        }
+        while (p != 0 && *s == *p) {
             s++;
             p++;
+        }
+        old = s;
+        old++;
+        p = pat;
     }
 }
 
 char *eliminados(char *str, char *pat) {
-  ...
+  return NULL;
 }
